7b48144dd64780806b86169b92eeaf341cd025e6
Anguiano <54717822+ajperezc@users.noreply.github.com>
2022-07-18 20:37:47 +0200

    Update files

---
+++ University/UniversityApiBackend/Models/DataModels/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class BaseEntity
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        public string CreatedBy { get; set; } = String.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string UpdatedBy { get; set; } = String.Empty;
+        public DateTime? UpdatedAt { get; set; } = DateTime.Now;
+        public string DeletedBy { get; set; } = String.Empty;
+        public DateTime? DeleteAt { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
